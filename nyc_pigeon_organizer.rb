eb07e63acf563dca15e2783028976f7a940474bc
steven nguyen <sqnguyen829@gmail.com>
2019-09-16 02:04:49 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,45 @@
+
+def nyc_pigeon_organizer(data)
+  # write your code here!
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+=begin
+
 def nyc_pigeon_organizer(data)
   new_hash = {}
   data.each do |key, value|
@@ -19,3 +61,5 @@ def nyc_pigeon_organizer(data)
   end
   new_hash
 end
+
+=end
